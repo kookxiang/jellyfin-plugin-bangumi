71ce50f21fc4f10dc7db0c530470b8802e1ac620
kookxiang <kookxiang@gmail.com>
2022-03-13 22:16:14 +0800

    add log

--- Jellyfin.Plugin.Bangumi/OAuth/PlaybackScrobbler.cs
+++ Jellyfin.Plugin.Bangumi/OAuth/PlaybackScrobbler.cs
@@ -7,24 +7,27 @@ using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.Plugins;
 using MediaBrowser.Controller.Session;
 using MediaBrowser.Model.Entities;
+using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Plugin.Bangumi.OAuth
 {
     public class PlaybackScrobbler : IServerEntryPoint
     {
-        private readonly ISessionManager _sessionManager;
-        private readonly IUserManager _userManager;
-        private readonly OAuthStore _store;
         private readonly BangumiApi _api;
+        private readonly ILogger<PlaybackScrobbler> _log;
+        private readonly ISessionManager _sessionManager;
+        private readonly OAuthStore _store;
+        private readonly IUserManager _userManager;
 
-        private Dictionary<Guid, HashSet<Guid>> _ignoreList = new();
+        private readonly Dictionary<Guid, HashSet<Guid>> _ignoreList = new();
 
-        public PlaybackScrobbler(ISessionManager sessionManager, IUserManager userManager, OAuthStore store, BangumiApi api)
+        public PlaybackScrobbler(ISessionManager sessionManager, IUserManager userManager, OAuthStore store, BangumiApi api, ILogger<PlaybackScrobbler> log)
         {
             _sessionManager = sessionManager;
             _userManager = userManager;
             _store = store;
             _api = api;
+            _log = log;
         }
 
         public void Dispose()
@@ -45,42 +48,55 @@ namespace Jellyfin.Plugin.Bangumi.OAuth
             if (!_ignoreList.ContainsKey(e.Session.UserId))
                 _ignoreList[e.Session.UserId] = new HashSet<Guid>();
             var user = _userManager.GetUserById(e.Session.UserId);
-            if (e.Item.IsPlayed(user))
-                _ignoreList[e.Session.UserId].Add(e.Item.Id);
+            if (!e.Item.IsPlayed(user))
+                return;
+            _log.LogInformation("item #{Id} has been played before, add to ignore list", e.Item.Id);
+            _ignoreList[e.Session.UserId].Add(e.Item.Id);
         }
 
         private void OnPlaybackStopped(object? sender, PlaybackStopEventArgs e)
         {
             var episodeId = e.MediaInfo.GetProviderId(Constants.ProviderName);
 
-            // only process files with bangumi id
-            if (string.IsNullOrEmpty(episodeId))
-                return;
+            if (!_ignoreList.ContainsKey(e.Session.UserId))
+                _ignoreList[e.Session.UserId] = new HashSet<Guid>();
 
-            // complete episode or finish 80% of the episode
-            if (!e.PlayedToCompletion || e.PlaybackPositionTicks > e.MediaInfo.RunTimeTicks * 0.8)
+            if (string.IsNullOrEmpty(episodeId))
+            {
+                _log.LogInformation("item #{Id} doesn't have bangumi id, ignored", e.Item.Id);
                 return;
+            }
+
+            if (!e.PlayedToCompletion || e.PlaybackPositionTicks > e.MediaInfo.RunTimeTicks * 0.8)
+            {
+                _log.LogInformation("item #{Id} haven't finish yet, ignored", e.Item.Id);
+                return;
+            }
 
             var user = _store.Get(e.Session.UserId);
-            // access token is required
             if (user == null)
+            {
+                _log.LogInformation("access token for user #{User} not found, ignored", e.Session.Id);
                 return;
+            }
 
-            // abort if access token is expired
             if (user.Expired)
+            {
+                _log.LogInformation("access token for user #{User} expired, ignored", e.Session.Id);
                 return;
+            }
 
-            // ignore if the episode is already played
-            if (_ignoreList.ContainsKey(e.Session.UserId) && _ignoreList[e.Session.UserId].Contains(e.Item.Id))
+            if (_ignoreList[e.Session.UserId].Contains(e.Item.Id))
+            {
+                _log.LogInformation("access token for user #{User} expired, ignored", e.Session.Id);
                 return;
+            }
 
-            // update episode status to bangumi
-            _api.UpdateEpisodeStatus(user.AccessToken, episodeId, EpisodeStatus.Watched, CancellationToken.None)
-                .ConfigureAwait(false);
+            _log.LogInformation("report episode #{Episode} status {Status} to bangumi", episodeId, EpisodeStatus.Watched);
+            _api.UpdateEpisodeStatus(user.AccessToken, episodeId, EpisodeStatus.Watched, CancellationToken.None).Wait();
 
-            // add episode to ignore list
-            if (_ignoreList.ContainsKey(e.Session.UserId))
-                _ignoreList[e.Session.UserId].Add(e.Item.Id);
+            _log.LogInformation("report completed, add episode to ignore list");
+            _ignoreList[e.Session.UserId].Add(e.Item.Id);
         }
     }
 }
