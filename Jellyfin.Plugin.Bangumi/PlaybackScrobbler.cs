5ae40d251d73db5683608fb819fed11d276366be
kookxiang <kookxiang@gmail.com>
2022-03-27 15:37:00 +0800

    fix scrobbler

--- Jellyfin.Plugin.Bangumi/PlaybackScrobbler.cs
+++ Jellyfin.Plugin.Bangumi/PlaybackScrobbler.cs
@@ -7,7 +7,6 @@ using Jellyfin.Plugin.Bangumi.Model;
 using Jellyfin.Plugin.Bangumi.OAuth;
 using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.Plugins;
-using MediaBrowser.Controller.Session;
 using MediaBrowser.Model.Entities;
 using Microsoft.Extensions.Logging;
 
@@ -19,13 +18,11 @@ namespace Jellyfin.Plugin.Bangumi
 
         private readonly BangumiApi _api;
         private readonly ILogger<PlaybackScrobbler> _log;
-        private readonly ISessionManager _sessionManager;
         private readonly OAuthStore _store;
         private readonly IUserDataManager _userDataManager;
 
-        public PlaybackScrobbler(ISessionManager sessionManager, IUserDataManager userDataManager, OAuthStore store, BangumiApi api, ILogger<PlaybackScrobbler> log)
+        public PlaybackScrobbler(IUserDataManager userDataManager, OAuthStore store, BangumiApi api, ILogger<PlaybackScrobbler> log)
         {
-            _sessionManager = sessionManager;
             _userDataManager = userDataManager;
             _store = store;
             _api = api;
@@ -35,68 +32,64 @@ namespace Jellyfin.Plugin.Bangumi
         public void Dispose()
         {
             _userDataManager.UserDataSaved -= OnUserDataSaved;
-            _sessionManager.PlaybackStopped -= OnPlaybackStopped;
             GC.SuppressFinalize(this);
         }
 
         public Task RunAsync()
         {
             _userDataManager.UserDataSaved += OnUserDataSaved;
-            _sessionManager.PlaybackStopped += OnPlaybackStopped;
             return Task.CompletedTask;
         }
 
         private void OnUserDataSaved(object? sender, UserDataSaveEventArgs e)
         {
-            if (e.SaveReason is not UserDataSaveReason.TogglePlayed)
-                return;
-
-            if (e.UserData.Played)
-                GetPlaybackHistory(e.UserId).Add(e.UserData.Key);
-            else
-                GetPlaybackHistory(e.UserId).Remove(e.UserData.Key);
-        }
-
-        private void OnPlaybackStopped(object? sender, PlaybackStopEventArgs e)
-        {
-            var episodeId = e.MediaInfo.GetProviderId(Constants.ProviderName);
-
-            if (string.IsNullOrEmpty(episodeId))
+            switch (e.SaveReason)
             {
-                _log.LogInformation("item {Name} (#{Id}) doesn't have bangumi id, ignored", e.Item.Name, e.Item.Id);
-                return;
+                case UserDataSaveReason.TogglePlayed when e.UserData.Played:
+                    GetPlaybackHistory(e.UserId).Add(e.UserData.Key);
+                    _log.LogInformation("mark {Name} (#{Id}) as played for user #{User}", e.Item.Name, e.Item.Id, e.UserId);
+                    break;
+
+                case UserDataSaveReason.TogglePlayed when !e.UserData.Played:
+                    GetPlaybackHistory(e.UserId).Remove(e.UserData.Key);
+                    _log.LogInformation("mark {Name} (#{Id}) as new for user #{User}", e.Item.Name, e.Item.Id, e.UserId);
+                    break;
+
+                case UserDataSaveReason.PlaybackFinished:
+                    var bangumiId = e.Item.GetProviderId(Constants.ProviderName);
+
+                    if (string.IsNullOrEmpty(bangumiId))
+                    {
+                        _log.LogInformation("item {Name} (#{Id}) doesn't have bangumi id, ignored", e.Item.Name, e.Item.Id);
+                        return;
+                    }
+
+                    var user = _store.Get(e.UserId);
+                    if (user == null)
+                    {
+                        _log.LogInformation("access token for user #{User} not found, ignored", e.UserId);
+                        return;
+                    }
+
+                    if (user.Expired)
+                    {
+                        _log.LogInformation("access token for user #{User} expired, ignored", e.UserId);
+                        return;
+                    }
+
+                    if (e.Item.GetUserDataKeys().Intersect(GetPlaybackHistory(e.UserId)).Any())
+                    {
+                        _log.LogInformation("item {Name} (#{Id}) has been played before, ignored", e.Item.Name, e.Item.Id);
+                        return;
+                    }
+
+                    _log.LogInformation("report episode #{Episode} status {Status} to bangumi", bangumiId, EpisodeStatus.Watched);
+                    _api.UpdateEpisodeStatus(user.AccessToken, bangumiId, EpisodeStatus.Watched, CancellationToken.None).Wait();
+
+                    _log.LogInformation("report completed");
+                    e.Item.GetUserDataKeys().ForEach(key => GetPlaybackHistory(e.UserId).Add(key));
+                    break;
             }
-
-            if (!e.PlayedToCompletion && e.PlaybackPositionTicks < e.MediaInfo.RunTimeTicks * 0.8)
-            {
-                _log.LogInformation("item {Name} (#{Id}) haven't finish yet, ignored", e.Item.Name, e.Item.Id);
-                return;
-            }
-
-            var user = _store.Get(e.Session.UserId);
-            if (user == null)
-            {
-                _log.LogInformation("access token for user #{User} not found, ignored", e.Session.Id);
-                return;
-            }
-
-            if (user.Expired)
-            {
-                _log.LogInformation("access token for user #{User} expired, ignored", e.Session.Id);
-                return;
-            }
-
-            if (e.Item.GetUserDataKeys().Intersect(GetPlaybackHistory(e.Session.UserId)).Any())
-            {
-                _log.LogInformation("item {Name} (#{Id}) has been played before, ignored", e.Item.Name, e.Item.Id);
-                return;
-            }
-
-            _log.LogInformation("report episode #{Episode} status {Status} to bangumi", episodeId, EpisodeStatus.Watched);
-            _api.UpdateEpisodeStatus(user.AccessToken, episodeId, EpisodeStatus.Watched, CancellationToken.None).Wait();
-
-            _log.LogInformation("report completed");
-            e.Item.GetUserDataKeys().ForEach(key => GetPlaybackHistory(e.Session.UserId).Add(key));
         }
 
         private HashSet<string> GetPlaybackHistory(Guid userId)
