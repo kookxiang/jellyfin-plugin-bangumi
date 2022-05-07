c6deb0be043f6d48afe75ddcb014d07be56edb49
kookxiang <kookxiang@gmail.com>
2022-05-08 01:09:40 +0800

    update build script

--- build.ps1
+++ build.ps1
@@ -1,45 +1,47 @@
-﻿$PluginInfo = @{
-    category = "Metadata"
-    description = "bgm.tv metadata provider for Jellyfin"
-    guid = "41b59f1b-a6cf-474a-b416-785379cbd856"
-    name = "Bangumi"
-    overview = "Jellyfin bgm.tv 数据源插件，用于拉取中文番剧信息及图片。"
-    owner = "kookxiang"
-    versions = @()
+﻿if ($env:PERSONAL_ACCESS_TOKEN) {
+    $PersonalAccessToken = $env:PERSONAL_ACCESS_TOKEN
+} elseif (Test-Path -Path "token") {
+    $PersonalAccessToken = (Get-Content "token")
+} else {
+    Write-Host 'No Personal Access Token found'
 }
 
-$Request = curl https://api.github.com/repos/kookxiang/jellyfin-plugin-bangumi/releases
+$PluginList = Get-Content repository.json | ConvertFrom-Json
+$PluginInfo = $PluginList[0]
+
+$Request = curl -Headers @{"Authorization" = "token $PersonalAccessToken"} https://api.github.com/repos/kookxiang/jellyfin-plugin-bangumi/releases
 $Releases = $Request.Content | ConvertFrom-Json
-$targetAbi = "10.7.0.0"
+$targetAbi = "10.8.0.0"
 foreach ($Release in $Releases) {
     $version = $Release.tag_name
-    if (!(Test-Path -Path "release/$version.zip")) {
-        Invoke-WebRequest -Uri $Release.assets[0].browser_download_url -OutFile $Release.assets[0].name
-        @{
-            category = "Metadata"
-            changelog = $Release.body
-            description = "Jellyfin bgm.tv 数据源插件，用于拉取中文番剧信息及图片。"
-            guid = "41b59f1b-a6cf-474a-b416-785379cbd856"
-            name = "Bangumi"
-            overview = "bgm.tv metadata provider for Jellyfin"
-            owner = "kookxiang"
-            targetAbi = $targetAbi
-            timestamp = $Release.published_at
-            version = "$version.0"
-        } | ConvertTo-Json -Compress | Out-File "meta.json" -Encoding UTF8 -NoNewline
-        Compress-Archive -LiteralPath $Release.assets[0].name, "meta.json" -DestinationPath "release/$version.zip"
-        Remove-Item -Path $Release.assets[0].name
-        Remove-Item -Path "meta.json"
+    if (Test-Path -Path "release/$version.zip") {
+        continue
     }
+    Invoke-WebRequest -Uri $Release.assets[0].browser_download_url -OutFile $Release.assets[0].name
+    @{
+        category = "Metadata"
+        changelog = $Release.body
+        description = "Jellyfin bgm.tv 数据源插件，用于拉取中文番剧信息及图片。"
+        guid = "41b59f1b-a6cf-474a-b416-785379cbd856"
+        name = "Bangumi"
+        overview = "bgm.tv metadata provider for Jellyfin"
+        owner = "kookxiang"
+        targetAbi = $targetAbi
+        timestamp = $Release.published_at
+        version = "$version.0"
+    } | ConvertTo-Json -Compress | Out-File "meta.json" -Encoding UTF8 -NoNewline
+    Compress-Archive -LiteralPath $Release.assets[0].name, "meta.json" -DestinationPath "release/$version.zip"
+    Remove-Item -Path $Release.assets[0].name
+    Remove-Item -Path "meta.json"
 
-    $PluginInfo.versions += @{
+    $PluginInfo.versions = @(@{
         checksum =  (Get-FileHash -Algorithm MD5 "release/$version.zip").Hash.ToLower()
         changelog = $Release.body
         targetAbi = $targetAbi
         sourceUrl = "https://jellyfin-plugin-bangumi.pages.dev/release/$version.zip"
         timestamp = $Release.published_at
         version = "$version.0"
-    }
+    }) + $PluginInfo.versions
 }
 
 ConvertTo-Json @($PluginInfo) -Compress -Depth 3 | Out-File "repository.json" -Encoding UTF8 -NoNewline
