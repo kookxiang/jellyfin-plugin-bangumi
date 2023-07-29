ef710bf00b28c5ce8a57f031a71f85be93fb968c
Mrs4s <mrs4sxiaoshi@gmail.com>
2023-07-30 03:27:29 +0800

    feat: ignore less than 5min file

--- Jellyfin.Plugin.Bangumi/Plugin.cs
+++ Jellyfin.Plugin.Bangumi/Plugin.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using HarmonyLib;
 using Jellyfin.Plugin.Bangumi.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
+using Newtonsoft.Json;
 
 namespace Jellyfin.Plugin.Bangumi;
 
 public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
     public static Plugin? Instance;
+    
+    public Dictionary<string, long> MediaTicks = new();
 
     public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
     {
         Instance = this;
+        LoadCache();
+        var harmony = new Harmony("jellyfin.plugin.bangumi");
+        harmony.PatchAll();
     }
 
     /// <inheritdoc />
@@ -37,5 +45,21 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
                 EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.ConfigPage.html"
             }
         };
+    }
+
+    public void LoadCache()
+    {
+        var ticksPath = Path.Combine(ApplicationPaths.CachePath, "bangumi", "ticks.json");
+        if (File.Exists(ticksPath))
+            MediaTicks = JsonConvert.DeserializeObject<Dictionary<string, long>>(File.ReadAllText(ticksPath));
+    }
+    
+    public void SaveCache()
+    {
+        var json = JsonConvert.SerializeObject(MediaTicks);
+        var path = Path.Combine(ApplicationPaths.CachePath, "bangumi");
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+        File.WriteAllText(Path.Combine(path, "ticks.json"), json);
     }
 }
