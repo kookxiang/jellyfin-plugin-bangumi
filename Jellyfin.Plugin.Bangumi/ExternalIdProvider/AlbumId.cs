5116f7a3d87ab298b6a1fd73ef822791cb8d3dfe
kookxiang <kookxiang@gmail.com>
2024-03-17 15:00:19 +0800

    rename external id providers

--- Jellyfin.Plugin.Bangumi/ExternalIdProvider/AlbumId.cs
+++ Jellyfin.Plugin.Bangumi/ExternalIdProvider/AlbumId.cs
@@ -1,22 +1,22 @@
-using MediaBrowser.Controller.Entities.Movies;
+using MediaBrowser.Controller.Entities.Audio;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
 using MediaBrowser.Model.Providers;
 
-namespace Jellyfin.Plugin.Bangumi.Extension;
+namespace Jellyfin.Plugin.Bangumi.ExternalIdProvider;
 
-public class ExternalMovieId : IExternalId
+public class AlbumId : IExternalId
 {
     public bool Supports(IHasProviderIds item)
     {
-        return item is Movie;
+        return item is MusicAlbum;
     }
 
     public string ProviderName => Constants.ProviderName;
 
     public string Key => Constants.ProviderName;
 
-    public ExternalIdMediaType? Type => ExternalIdMediaType.Series;
+    public ExternalIdMediaType? Type => ExternalIdMediaType.Album;
 
     public string UrlFormatString => "https://bgm.tv/subject/{0}";
 }
