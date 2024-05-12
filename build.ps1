9649962c8102898da6b471d31af7dfd989585fc3
kookxiang <kookxiang@gmail.com>
2024-05-12 15:04:44 +0800

    Release 1.6.0

--- build.ps1
+++ build.ps1
@@ -1,4 +1,6 @@
-﻿if ($env:PERSONAL_ACCESS_TOKEN) {
+﻿Set-Location $PSScriptRoot
+
+if ($env:PERSONAL_ACCESS_TOKEN) {
     $PersonalAccessToken = $env:PERSONAL_ACCESS_TOKEN
 } elseif (Test-Path -Path "token") {
     $PersonalAccessToken = (Get-Content "token")
@@ -9,15 +11,18 @@
 $PluginList = Get-Content repository.json | ConvertFrom-Json
 $PluginInfo = $PluginList[0]
 
-$Request = curl -Headers @{"Authorization" = "token $PersonalAccessToken"} https://api.github.com/repos/kookxiang/jellyfin-plugin-bangumi/releases
+$Request = Invoke-WebRequest -Headers @{"Authorization" = "token $PersonalAccessToken"} https://api.github.com/repos/kookxiang/jellyfin-plugin-bangumi/releases
 $Releases = $Request.Content | ConvertFrom-Json
-$targetAbi = "10.8.0.0"
+$targetAbi = "10.9.0.0"
 foreach ($Release in $Releases) {
     $version = $Release.tag_name
     if (Test-Path -Path "release/$version.zip") {
         continue
     }
     foreach ($asset in $Release.assets) {
+        if ($asset.name -notlike "Jellyfin*.zip") {
+            continue
+        }
         Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $asset.name
     }
     @{
@@ -32,7 +37,7 @@ foreach ($Release in $Releases) {
         timestamp = $Release.published_at
         version = "$version.0"
     } | ConvertTo-Json -Compress | Out-File "meta.json" -Encoding UTF8 -NoNewline
-    mv $asset.name "release/$version.zip"
+    Move-Item $asset.name "release/$version.zip"
     Compress-Archive -LiteralPath "meta.json" -Update -DestinationPath "release/$version.zip"
     Remove-Item -Path "meta.json"
 
