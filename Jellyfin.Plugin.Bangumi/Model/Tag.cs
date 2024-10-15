454e2856abf1c171ce1891af9cf58d5d9f3b6f07
kookxiang <kookxiang@gmail.com>
2024-10-15 23:43:54 +0800

    fix tag

--- Jellyfin.Plugin.Bangumi/Model/Tag.cs
+++ Jellyfin.Plugin.Bangumi/Model/Tag.cs
@@ -1,8 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Jellyfin.Plugin.Bangumi.Model;
 
 public class Tag
 {
+    [JsonPropertyName("name")]
     public string Name { get; set; } = null!;
 
+    [JsonPropertyName("count")]
     public int Count { get; set; }
 }
