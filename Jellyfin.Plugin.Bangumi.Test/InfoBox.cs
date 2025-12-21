3d55c0ff29bb40ae2673fad2794cc70b8cfcaa0c
kookxiang <kookxiang@gmail.com>
2025-12-21 21:19:08 +0800

    test: update DuplicatedKey test

--- Jellyfin.Plugin.Bangumi.Test/InfoBox.cs
+++ Jellyfin.Plugin.Bangumi.Test/InfoBox.cs
@@ -1,6 +1,6 @@
-using System.Threading;
+using System.Text.Json;
 using System.Threading.Tasks;
-using Jellyfin.Plugin.Bangumi.Test.Util;
+using Jellyfin.Plugin.Bangumi.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Jellyfin.Plugin.Bangumi.Test;
@@ -8,18 +8,15 @@ namespace Jellyfin.Plugin.Bangumi.Test;
 [TestClass]
 public class InfoBoxTest
 {
-    private readonly BangumiApi _api = ServiceLocator.GetService<BangumiApi>();
-
-    private readonly CancellationToken _token = CancellationToken.None;
-
     [TestMethod]
-    [Ignore("条目已更新，暂时跳过测试")]
     public async Task DuplicatedKey()
     {
-        var subject = await _api.GetSubject(293049, _token);
-        Assert.AreEqual(subject?.InfoBox?.Get("音响"), "マジックカプセル；音响制作担当：見留滉平");
+        var data = JsonSerializer.Deserialize<JsonElement>("[{\"key\":\"中文名\",\"value\":\"间谍过家家\"},{\"key\":\"脚本\",\"value\":\"山崎莉乃、谷村大四郎、河口友美、加藤穂乃伽\"},{\"key\":\"脚本\",\"value\":\"河口友美(1,5,7)、山崎莉乃(2,4,8,11)、谷村大四郎(3,6,10,12)、加藤穂乃伽(9)\"}]", Constants.JsonSerializerOptions);
+        var infobox = InfoBox.ParseJson(data);
+        Assert.AreEqual(infobox.Get("脚本"), "河口友美(1,5,7)、山崎莉乃(2,4,8,11)、谷村大四郎(3,6,10,12)、加藤穂乃伽(9)");
 
-        subject = await _api.GetSubject(374319, _token);
-        Assert.AreEqual(subject?.InfoBox?.Get("中文名"), "无意间变成狗，被喜欢的女生捡回家。");
+        data = JsonSerializer.Deserialize<JsonElement>("[{\"key\":\"中文名\",\"value\":\"无意间变成狗，被喜欢的女生捡回家。\"},{\"key\":\"别名\",\"value\":[{\"k\":\"非官方\",\"v\":\"生而为狗，我很幸福\"},{\"k\":\"非官方\",\"v\":\"变成狗后被喜欢的人捡了。\"},{\"v\":\"Inu ni Nattara Suki na Hito ni Hirowareta.\"}]}]", Constants.JsonSerializerOptions);
+        infobox = InfoBox.ParseJson(data);
+        Assert.AreEqual(infobox.Get("中文名"), "无意间变成狗，被喜欢的女生捡回家。");
     }
 }
