43ccfdfa647805af419a92369d97b421769e23ba
wsgh0202 <wsotl7102@outlook.com>
2026-03-10 00:52:31 +0800

    test: 移除错误测试，排序错误是由于默认算法缺少传入别名，与算法本身效果无关

--- Jellyfin.Plugin.Bangumi.Test/Series.cs
+++ Jellyfin.Plugin.Bangumi.Test/Series.cs
@@ -229,10 +229,6 @@ public class Series
         _plugin.Configuration.SortByFuzzScore = true;
         searchResults = await _api.SearchSubject("The Adventures of Tom Sawyer", _token);
         Assert.AreEqual(searchResults.First().Id, 23611, "should return most similar item as first");
-        // 原排序错误匹配: The Adventures of Mark Twain, id:113269
-        _plugin.Configuration.SortByFuzzScore = false;
-        searchResults = await _api.SearchSubject("The Adventures of Tom Sawyer", _token);
-        Assert.AreNotEqual(searchResults.First().Id, 23611, "should return most similar item as first");
     }
 
     [TestMethod]
